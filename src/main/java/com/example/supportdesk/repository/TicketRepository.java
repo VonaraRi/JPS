8e584e43ca8b0becbfe4bb05b5fae2705ff059b1
VonaraRi <rionnalynrichard5@gmail.com>
2026-07-10 01:03:19 +0800

    Exercise 7.2

---
+++ src/main/java/com/example/supportdesk/repository/TicketRepository.java
@@ -0,0 +1,10 @@
+package com.example.supportdesk.repository;
+
+import com.example.supportdesk.model.Ticket;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends MongoRepository<Ticket, String> {
+    // This interface automatically inherits all MongoDB CRUD methods (save, find, delete, etc.)
+}
