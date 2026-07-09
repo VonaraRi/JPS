8083108f4e5e2de4b116529388b5d205d950a068
VonaraRi <rionnalynrichard5@gmail.com>
2026-07-10 01:14:57 +0800

    Exercise 7.2

--- src/main/java/com/example/AssetTracker/AssetTrackerApplication.java
+++ src/main/java/com/example/AssetTracker/AssetTrackerApplication.java
@@ -2,12 +2,12 @@ package com.example.AssetTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.example.supportdesk.repository")
 public class AssetTrackerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AssetTrackerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AssetTrackerApplication.class, args);
+    }
 }
