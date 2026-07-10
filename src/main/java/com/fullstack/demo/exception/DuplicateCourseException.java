ff48dffeeca34d186a5926dab2f49f07d05162ad
VonaraRi <rionnalynrichard5@gmail.com>
2026-07-10 12:55:36 +0800

    updated

--- src/main/java/com/fullstack/demo/exception/DuplicateCourseException.java
+++
@@ -1,7 +0,0 @@
-package src.main.java.com.fullstack.demo.exception;
-
-public class DuplicateCourseException extends RuntimeException{
-    public DuplicateCourseException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/fullstack/demo/exception/DuplicateStudentException.java
+++
@@ -1,7 +0,0 @@
-package src.main.java.com.fullstack.demo.exception;
-
-public class DuplicateStudentException extends RuntimeException {
-    public DuplicateStudentException(String message) {
-        super(message);
-    }
-}
